2b4dea70f11d6c0767c6ce349d41cdb20d1013ad
Galang0304 <1058441117422@student.unismuh.ac.id>
2025-12-28 23:33:21 +0800

    Add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kurirta-api',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      }
+    },
+    {
+      name: 'kurirta-bot',
+      script: 'bot/whatsapp-bot.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'kurirta-client',
+      script: 'serve',
+      args: '-s client/build -l 3000',
+      instances: 1,
+      autorestart: true,
+      watch: false
+    }
+  ]
+};
